b5e2dc4580e80ae38e16812c1b44370096474a78
aaron-might <aaronmighty7@gmail.com>
2021-08-27 19:12:48 -0400

    declaring the variable in global state

---
+++ index.js
@@ -0,0 +1,6 @@
+const form = documemt.getElementById('form');
+const bill= documemt.getElementById('bill');
+const customer= documemt.getElementById('customer')
+const tipAmount= documemt.getElementById('tipAmount')
+const totalAmount= documemt.getElementById('totalAmount')
+const people= documemt.getElementById('people')
